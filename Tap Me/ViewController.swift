b6b2be4703675c148ee4ccb74461676b27760039
McPusz <mcpusz@mcpuszs-macbook-pro.local>
2016-05-08 21:45:23 +0200

    Dziala wszystko, bez obrazkow

--- Tap Me/ViewController.swift
+++ Tap Me/ViewController.swift
@@ -14,21 +14,60 @@ class ViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     
     var count = 0
+    var seconds = 0
+    var timer = NSTimer()
     
     @IBAction func buttonPressed(){ /* pressing a button */
         
-        count++
+        count += 1
     
         
         scoreLabel.text = "Score \n\(count)"
         NSLog("Button Pressed")
         
     }
+    
+    func subtractTime(){    /* decrement seconds, update timeLabel, alert pop-up */
+        
+        seconds -= 1
+        timerLabel.text = "Time: \(seconds)"
+        
+        if (seconds == 0) {
+            
+            timer.invalidate()  /* koniec pomiaru sekund */
+            
+            let alert = UIAlertController(title: "Time's up!", message: "You scored \(count) points", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "Play Again", style: UIAlertActionStyle.Default, handler: {
+                    action in self.setupGame()
+            }))
+            
+            presentViewController(alert, animated: true, completion: nil)
+            
+        } /* if */
+        
+    }
+    
+    func setupGame(){   /* set time/score */
+        
+        seconds = 30
+        count = 0
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("subtractTime"), userInfo: nil, repeats: true)
+        
+        timerLabel.text = "Time: \(seconds)"
+        scoreLabel.text = "Score: \(count)"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupGame()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
