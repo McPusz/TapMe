1fb4960a7efc5cd9f8937cdfc1f54250f4d21916
McPusz <mcpusz@mcpuszs-macbook-pro.local>
2016-05-21 16:16:19 +0200

    Wersja w kotki. Background i t¬o do label do poprawy

--- Tap Me/ViewController.swift
+++ Tap Me/ViewController.swift
@@ -1,17 +1,25 @@
 //
 //  ViewController.swift
-//  Tap Me
+//  Sujka The Game
 //
 //  Created by McPusz on 08/05/16.
 //  Copyright © 2016 McPusz. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
     
     @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var timerLabel: UILabel!
+    
+    var buttonBeep: AVAudioPlayer?
+    var secondBeep : AVAudioPlayer?
+    var backgroundMusic : AVAudioPlayer?
+    
+    var beepPlayer: AVAudioPlayer?
     
     var count = 0
     var seconds = 0
@@ -24,6 +32,7 @@ class ViewController: UIViewController {
         
         scoreLabel.text = "Score \n\(count)"
         NSLog("Button Pressed")
+        buttonBeep?.play()
         
     }
     
@@ -31,6 +40,8 @@ class ViewController: UIViewController {
         
         seconds -= 1
         timerLabel.text = "Time: \(seconds)"
+        
+        secondBeep?.play()
         
         if (seconds == 0) {
             
@@ -46,6 +57,7 @@ class ViewController: UIViewController {
             
         } /* if */
         
+        
     }
     
     func setupGame(){   /* set time/score */
@@ -57,11 +69,72 @@ class ViewController: UIViewController {
         
         timerLabel.text = "Time: \(seconds)"
         scoreLabel.text = "Score: \(count)"
+        
+        backgroundMusic?.volume = 0.3
+        backgroundMusic?.play()
+        
     }
+   
+    func playSoundFile(){
+        if let myAssetSound = NSDataAsset(name:"buttonBeep"){
+            let soundFile = myAssetSound.data
+            
+            do{
+                try beepPlayer = AVAudioPlayer (data: soundFile)
+            } catch{
+                print("NO AUDIO PLAYER")
+            }
+            beepPlayer!.play()
+        }
+    }
+    
+    
+    
+//    func setupAudioPlayerWithFile(file: NSString, type: NSString)-> AVAudioPlayer? {
+//        
+//        //1
+//        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+//        let url = NSURL.fileURLWithPath(path!)
+//        
+//        //2
+//        var audioPlayer: AVAudioPlayer?
+//        
+//        //3
+//        do{
+//            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+//        }catch{
+//            print("Player not available")
+//        }
+//        
+//        return audioPlayer
+//        
+//    }
 
     override func viewDidLoad() {
+        
+//        if let buttonBeep = self.setupAudioPlayerWithFile("ButtonTap", type: "wav"){
+//            self.buttonBeep = buttonBeep
+//        }
+//        
+//        if let secondBeep = self.setupAudioPlayerWithFile("SecondBeep", type: "wav"){
+//            self.secondBeep = secondBeep
+//        }
+//        
+//        if let backgroundMusic = self.setupAudioPlayerWithFile("BackgroundMusic", type: "wav"){
+//            self.backgroundMusic = backgroundMusic
+//        }
+        
+        
+        
         super.viewDidLoad()
         setupGame()
+        
+        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_1.png")!)
+        
+        scoreLabel.backgroundColor = UIColor( patternImage: UIImage(named: "field_score.png")!)
+        
+        timerLabel.backgroundColor = UIColor (patternImage: UIImage(named: "field_time.png")!)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
